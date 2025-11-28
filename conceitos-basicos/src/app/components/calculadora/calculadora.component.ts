b23a7692b5ec5e543fbaa23185a22d08ac2b4b23
KennedyMartinsB <kenedy.martins185@gmail.com>
2025-11-28 00:56:28 -0300

    Feature: ajustes de condicionais

--- conceitos-basicos/src/app/components/calculadora/calculadora.component.ts
+++ conceitos-basicos/src/app/components/calculadora/calculadora.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-calculadora',
   templateUrl: './calculadora.component.html',
   styleUrls: ['./calculadora.component.scss']
 })
 export class CalculadoraComponent {
+
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  calcularResultado(){
+    console.log("chamando método de calcular resultado");
+    this.resultado = this.numero1 + this.numero2;
+  }
 
 }
