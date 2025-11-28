9ca23e229d117c8b277d941022c0f3575120e8a5
KennedyMartinsB <kenedy.martins185@gmail.com>
2025-11-28 14:54:09 -0300

    Feature: criação do componente de tabela

--- conceitos-basicos/src/app/app-routing.module.ts
+++ conceitos-basicos/src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CalculadoraComponent } from './components/calculadora/calculadora.component';
+import { ListaComprasComponent } from './components/lista-compras/lista-compras.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'calculadora',
+    pathMatch: 'full'
+  },
+
+  {
+    path: 'calculadora',
+    component: CalculadoraComponent
+  },
+
+  {
+    path: 'lista',
+    component: ListaComprasComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ conceitos-basicos/src/app/components/lista-compras/lista-compras.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-compras',
+  templateUrl: './lista-compras.component.html',
+  styleUrls: ['./lista-compras.component.scss']
+})
+export class ListaComprasComponent {
+
+}
