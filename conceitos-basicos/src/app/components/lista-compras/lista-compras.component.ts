e43077375c878b2df0141b227aaaeb43b582d7fb
KennedyMartinsB <kenedy.martins185@gmail.com>
2025-12-01 17:35:35 -0300

    Feature: limpar lista de compras e marcar item como comprado

--- conceitos-basicos/src/app/components/lista-compras/lista-compras.component.ts
+++ conceitos-basicos/src/app/components/lista-compras/lista-compras.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ItemLista } from '../../interface/item-lista';
 
 @Component({
   selector: 'app-lista-compras',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./lista-compras.component.scss']
 })
 export class ListaComprasComponent {
+  item: string = '';
+  lista: ItemLista[] = [];
 
+  adicionarItem(){
+    // console.log("Item recebido: ", this.item)
+    let itemLista = new ItemLista();
+    itemLista.nome = this.item;
+    itemLista.id = this.lista.length + 1;
+    this.lista.push(itemLista);
+    this.item = '';
+
+
+  }
+
+  marcarItem(ItemLista: ItemLista) {
+    ItemLista.comprado = !ItemLista.comprado;
+  }
+
+  limparLista(){
+    this.lista = [];
+  }
 }
