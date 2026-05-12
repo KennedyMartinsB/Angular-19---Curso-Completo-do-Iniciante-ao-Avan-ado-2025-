4dc0e67660c4e7cdae30443ee51e41a08bac79de
KennedyMartinsB <kenedy.martins185@gmail.com>
2026-05-12 16:13:16 -0300

    Fix: ajustes aula 36

--- crud-angular-material/src/app/components/consulta/consulta.component.ts
+++ crud-angular-material/src/app/components/consulta/consulta.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/interface/cliente';
 import { ClienteService } from 'src/app/service/cliente.service';
 
@@ -7,13 +7,14 @@ import { ClienteService } from 'src/app/service/cliente.service';
   templateUrl: './consulta.component.html',
   styleUrls: ['./consulta.component.scss']
 })
-export class ConsultaComponent {
+export class ConsultaComponent implements OnInit{
 
   clientList: Cliente[] = [];
 
   constructor(private clientService: ClienteService){ }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    // console.log("Passando pelo OnInit")
     this.clientList = this.clientService.searchClient('');
   }
 
