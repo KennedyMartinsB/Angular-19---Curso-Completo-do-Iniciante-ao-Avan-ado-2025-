9c9eeda8bc01f0c6cb1ffe755f5381caf1cf4043
KennedyMartinsB <kenedy.martins185@gmail.com>
2026-08-08 20:34:38 -0300

    Feature: configuração de formulário reativo

--- passeio-app/src/app/lugares/lugar/lugar.component.ts
+++ passeio-app/src/app/lugares/lugar/lugar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Categoria } from 'src/app/categorias/categoria';
 
 @Component({
   selector: 'app-lugar',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./lugar.component.scss']
 })
 export class LugarComponent {
+  camposForm: FormGroup;
+  categorias: Categoria[] = [];
 
+  constructor() {
+    this.camposForm = new FormGroup({
+      nome: new FormControl('', Validators.required),
+      categorias: new FormControl('', Validators.required),
+      localizacao: new FormControl('', Validators.required),
+      urlFoto: new FormControl('', Validators.required),
+      avaliacao: new FormControl('', Validators.required),
+    })
+  }
+
+  salvar() {
+    console.log("Valores: ", this.camposForm.value)
+  }
 }
+
+
+// nome?: string;
+//   categoria?: string;
+//   localização?: string;
+//   urlFoto?: string;
+//   avaliacao?: number;
