a98cc37aff01555e99f0147e441b7c52e81b0691
KennedyMartinsB <kenedy.martins185@gmail.com>
2026-08-23 04:29:06 -0300

    Feature: listagem de categorias na tela de galeria

--- passeio-app/src/app/galeria/galeria/galeria.component.ts
+++ passeio-app/src/app/galeria/galeria/galeria.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Categoria } from 'src/app/categorias/categoria';
+import { CategoriaService } from 'src/app/categorias/categoria.service';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
   styleUrls: ['./galeria.component.scss']
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit{
+  categoria: Categoria[] = []
 
+  ngOnInit() {
+    this.carregarCategorias()
+  }
+
+  constructor(private categoriasService: CategoriaService) {}
+
+  carregarCategorias() {
+    this.categoriasService.obterTodas().subscribe({
+      next: (categorias) => {
+        console.log('Categorias Recebidas: ', categorias)
+        this.categoria = categorias
+      },
+      error: erro => console.error('Houve um erro ao carregar categorias: ', erro)
+    })
+  }
 }
