f025f63f010121ff4c87ec8aa40b52d768a4fbb3
KennedyMartinsB <kenedy.martins185@gmail.com>
2026-08-26 18:37:35 -0300

    Feature: renderização dos cards e listagem de categirias

--- passeio-app/src/app/galeria/galeria/galeria.component.ts
+++ passeio-app/src/app/galeria/galeria/galeria.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriaService } from 'src/app/categorias/categoria.service';
 import { LugarService } from 'src/app/lugares/lugar.service';
 import { Lugar } from '../../lugares/lugar';
+import { Categoria } from 'src/app/categorias/categoria';
 
 @Component({
   selector: 'app-galeria',
@@ -9,21 +10,33 @@ import { Lugar } from '../../lugares/lugar';
   styleUrls: ['./galeria.component.scss']
 })
 export class GaleriaComponent implements OnInit{
-  lugar: Lugar[] = []
+  categoriasFiltro: Categoria[] = [];
+  lugares: Lugar[] = [];
 
   ngOnInit() {
-    this.carregarCategorias()
+    this.carregarCategorias();
+    this.carregarLugares();
   }
 
-  constructor(private lugarService: LugarService) {}
+  constructor(private lugarService: LugarService, private categoriaService: CategoriaService) {}
 
   carregarCategorias() {
     this.lugarService.obterTodos().subscribe({
-      next: (lugares) => {
-        console.log('Categorias Recebidas: ', lugares)
-        this.lugar = lugares
+      next: (categorias) => {
+        console.log('Categorias Recebidas: ', categorias)
+        this.categoriasFiltro = categorias
       },
       error: erro => console.error('Houve um erro ao carregar categorias: ', erro)
+    })
+  }
+
+  carregarLugares() {
+    this.lugarService.obterTodos().subscribe({
+      next: (lugaresList) => {
+        console.log('Lista De Lugares Recebida: ', lugaresList)
+        this.lugares = lugaresList
+      },
+      error: erro => console.error('Houve um erro ao carregar a lista de lugares: ', erro)
     })
   }
 }
