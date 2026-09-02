f16b5fe8da663218cd3ddb55be654f09a68e572b
KennedyMartinsB <kenedy.martins185@gmail.com>
2026-09-02 18:56:58 -0300

    Feature: titulo e subtitulo dinamico

--- passeio-app/src/app/template/layout/layout.component.ts
+++ passeio-app/src/app/template/layout/layout.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LayoutProps } from './layoutprops';
+import { ActivatedRoute, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-layout',
@@ -6,6 +9,25 @@ import { Component } from '@angular/core';
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss']
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit{
+  props: LayoutProps = { titulo: '', subTitulo: '' };
 
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void{
+    // this.props = this.obterPropriedadesDeLayout();
+    this.router.events.pipe(
+      filter(() => this.activatedRoute.firstChild !== null),
+      map(() => this.obterPropriedadesDeLayout())
+    ).subscribe((props: LayoutProps) => this.props = props)
+  }
+
+  obterPropriedadesDeLayout() : LayoutProps {
+    let rotaFilha = this.activatedRoute.firstChild;
+
+    while(rotaFilha?.firstChild){
+      rotaFilha = rotaFilha.firstChild
+    }
+    return rotaFilha?.snapshot.data as LayoutProps
+  }
 }
