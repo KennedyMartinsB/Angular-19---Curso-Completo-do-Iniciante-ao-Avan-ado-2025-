312dba045c0bb9cc5e08406c77929302cdd3cea6
KennedyMartinsB <kenedy.martins185@gmail.com>
2026-09-22 19:08:44 -0300

    Feature: controle de acesso as paginas

--- passeio-app/src/app/template/layout/layout.component.ts
+++ passeio-app/src/app/template/layout/layout.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { LayoutProps } from './layoutprops';
 import { ActivatedRoute, Router } from '@angular/router';
 import { filter, map } from 'rxjs';
+import { AuthgoogleService } from 'src/app/authgoogle.service';
 
 @Component({
   selector: 'app-layout',
@@ -12,7 +13,7 @@ import { filter, map } from 'rxjs';
 export class LayoutComponent implements OnInit{
   props: LayoutProps = { titulo: '', subTitulo: '' };
 
-  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private loginService: AuthgoogleService) {}
 
   ngOnInit(): void{
     // this.props = this.obterPropriedadesDeLayout();
@@ -29,5 +30,9 @@ export class LayoutComponent implements OnInit{
       rotaFilha = rotaFilha.firstChild
     }
     return rotaFilha?.snapshot.data as LayoutProps
+  }
+
+  logout() {
+    this.loginService.logout();
   }
 }
